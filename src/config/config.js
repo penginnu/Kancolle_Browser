80e1bcd2df8e390939fee2be3fd8565421395f34
pengin.nu <i.link0502@gmail.com>
2018-08-24 22:06:14 +0900

    electron を 2.8.0にアップデート

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,13 @@
+'use strict';
+
+const electron = require('electron');
+const fs = require('fs');
+const os = require('os');
+const path = require('path');
+
+var ipc = electron.ipcRenderer;
+const shell = electron.shell
+
 function ChangeTab(tabname) {
     // タブメニュー非表示
     document.getElementById('tab1').style = 'display: none;';
@@ -5,4 +15,13 @@ function ChangeTab(tabname) {
     document.getElementById('tab3').style = 'display: none;';
     // タブメニュー実装
     document.getElementById(tabname).style = 'display: block;';
+ }
+
+ function reloadCacheCrear () {
+    ipc.on('Reload-Cache-Delete-reply', function(responce) {
+        console.log("Reload-Cache-Crear Responce : " + responce);
+    });
+    ipc.send('Reload-Cache-Delete', 'RELOAD-CACHE-CREAR');
+
+    return 0;
  }
